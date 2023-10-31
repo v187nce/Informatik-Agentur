bfdbb946b2ac621bf9e6498f2a01ba1cb70a4ece
v187nce <134378283+v187nce@users.noreply.github.com>
2023-10-31 11:45:13 +0000

    implemented Umgekehrt.java to project

---
+++ Umgekehrt.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class Umgekehrt{
+
+    private Queue<Integer> zahlenfolge = new Queue<Integer>(); 
+    private Stack<Integer> ausgabe = new Stack<Integer>(); 
+
+    public Umgekehrt(){
+    }
+
+    public void ausgabeZahlenfolge(){
+        while(!zahlenfolge.isEmpty()){
+            ausgabe.push(zahlenfolge.front());
+            zahlenfolge.dequeue();
+        } 
+        while(!ausgabe.isEmpty()){
+            System.out.println(ausgabe.top() + " ");
+            ausgabe.pop();
+        }
+    }
+
+    public void eingabe(int eingabe){
+        zahlenfolge.enqueue(eingabe);
+    }
+
+}
